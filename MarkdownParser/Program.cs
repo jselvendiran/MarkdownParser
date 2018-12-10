c0e8af342cdd3e0d09b80ec7addcc8e867bc7530
Selvendiran Jayaraman <selvendiranj@hotmail.com>
2018-12-10 20:16:47 +0530

    markdown version history parser is complete

--- MarkdownParser/Program.cs
+++ MarkdownParser/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace MarkdownParser
 {
     class Program
     {
+        private static string mdFileName = "ReleaseHistory.md";
+        private static Regex versionExp = new Regex(@"##\s?\b\d{1,3}\.\d{1,3}\.\d{1,3}\b");
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GetReleaseHistory();
+        }
+
+        static void GetReleaseHistory()
+        {
+            string line;
+            Dictionary<string, string> vrnHistory = new Dictionary<string, string>();
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file = null;
+            try
+            {
+                file = new System.IO.StreamReader(mdFileName);
+                var key = string.Empty;
+                var value = string.Empty;
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    Match m = versionExp.Match(line);
+                    if (m.Success)
+                    {
+                        key = line;
+                        value = string.Empty;
+                        vrnHistory.Add(key, value);
+                    }
+                    else
+                    {
+                        vrnHistory[key] = value += line;
+                    }
+                }
+            }
+
+            finally
+            {
+                file?.Close();
+            }
         }
     }
 }
---
+++ MarkdownParser/ReleaseHistory.md
@@ -0,0 +1,9 @@
+﻿## 2.13.3
+* Release Notes 1
+## 2.13.2
+* Release Notes 1
+* Release Notes 2
+* Release Notes 3
+## 2.13.1
+* Release Notes 1
+* Release Notes 2
